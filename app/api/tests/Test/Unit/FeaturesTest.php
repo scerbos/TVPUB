6b4f8dae3193f48360f0f90f69eb09b2e528c490
Siroberto Scerbo <github@siroberto.com>
2014-10-08 22:41:26 -0400

    needed to stop testing errors

--- app/api/tests/Test/Unit/FeaturesTest.php
+++ app/api/tests/Test/Unit/FeaturesTest.php
@@ -14,8 +14,8 @@ class FeaturesTest extends \PHPUnit_Framework_TestCase
         );
         $features = new Features($allFeatures);
         $this->assertEquals(array(
-            array('id' => 'f1', 'name' => 'feature 1', 'href' => './api/features/f1'),
-            array('id' => 'f2', 'name' => 'feature 2', 'href' => './api/features/f2'),
+            array('id' => 'f1', 'name' => 'feature 1', 'desc' => 'description 1', 'href' => './api/features/f1'),
+            array('id' => 'f2', 'name' => 'feature 2', 'desc' => 'description 2', 'href' => './api/features/f2'),
         ), $features->getFeatures());
     }
 
