6284f6d9f884ea878babd25b15c576a329c27fa8
Siroberto Scerbo <github@siroberto.com>
2014-10-13 22:51:11 -0400

    basic skeleton for auth

---
+++ app/scripts/services/auth.js
@@ -0,0 +1,81 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name tvpubApp.auth
+ * @description
+ * # auth
+ * Factory in the tvpubApp.
+ */
+angular.module('tvpubApp')
+  .factory('Auth', function ($http, $cookieStore) {
+    var currentUser = {};
+    if($cookieStore.get('token')) {
+      currentUser = User.get();
+    }
+
+    // Public API here
+    return {
+      /**
+       * Authenticate user and save token
+       *
+       * @param  {Object}   user     - login info
+       * @param  {Function} callback - optional
+       * @return {Promise}
+       */
+      login: function(user, callback) {
+        var cb = callback || angular.noop;
+        var deferred = $q.defer();
+
+        $http.post('/auth/local', {
+          email: user.email,
+          password: user.password
+        }).
+        success(function(data) {
+          $cookieStore.put('token', data.token);
+          currentUser = User.get();
+          deferred.resolve(data);
+          return cb();
+        }).
+        error(function(err) {
+          this.logout();
+          deferred.reject(err);
+          return cb(err);
+        }.bind(this));
+
+        return deferred.promise;
+      },
+
+      /**
+       * Delete access token and user info
+       *
+       * @param  {Function}
+       */
+      logout: function() {
+        $cookieStore.remove('token');
+        currentUser = {};
+      },
+
+      /**
+       * Create a new user
+       *
+       * @param  {Object}   user     - user info
+       * @param  {Function} callback - optional
+       * @return {Promise}
+       */
+      createUser: function(user, callback) {
+        var cb = callback || angular.noop;
+
+        return User.save(user,
+          function(data) {
+            $cookieStore.put('token', data.token);
+            currentUser = User.get();
+            return cb(user);
+          },
+          function(err) {
+            this.logout();
+            return cb(err);
+          }.bind(this)).$promise;
+      },
+    };
+  });
